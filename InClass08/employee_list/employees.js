047f633c7b308824533bd782384f8c503fcbb293
James Leasure <james.leasure@tri-c.edu>
2017-10-31 13:59:53 -0400

    After class 08

--- InClass08/employee_list/employees.js
+++ InClass08/employee_list/employees.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$("#accordion").on("click", "h2", function() {
 		$(this).toggleClass("minus");
 		if ($(this).attr("class") != "minus") {
@@ -7,14 +7,26 @@ $(document).ready(function() {
 	   	else {
 	        $(this).next().show();
 	   	}
-    }); // end click
+	}); // end click
+
+	$("div").mouseover(function () {
+	    if ($(this).parent().is("h1")) {
+	        $(this).unwrap();
+	    } else {
+	        $(this).wrap("<h1></h1>");
+	    }
+	    alert($(this).next().html());
+	}); 
     
     $("#add").click(function() {
     	var html = "";
     	html += "<h2><a href='#'>" + $("#name").val() + "</a></h2>";
     	html += "<div><h3>" + $("#position").val() + "</h3>";
     	html += "<p>" + $("#description").val() + "</p></div>";
-    	$("#accordion").append(html);
+
+    	//$("div:eq(0)").after(html);
+    	$("div").first().after(html);
+
     	$("#name").val("");
     	$("#position").val("");
     	$("#description").val("");
